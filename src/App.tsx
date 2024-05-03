e88210af6c057e904987b7ca8e6112b824aa40e6
Satyam Sagar <satyamsagar30@gmail.com>
2024-05-04 04:49:55 +0530

    feat: added jobcard ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { Box, Grid, ThemeProvider, createTheme } from "@mui/material";
+import "@fontsource-variable/lexend";
+import JobCard from "./Components/JobCard";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Lexend Variable", sans-serif',
+  },
+  palette: {
+    primary: {
+      main: "#55efc4",
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ThemeProvider theme={theme}>
+        <Box className="container-view">
+          <Grid container spacing="30px">
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+            <Grid item xs={4}>
+              <JobCard />
+            </Grid>
+          </Grid>
+        </Box>
+      </ThemeProvider>
     </>
   );
 }
